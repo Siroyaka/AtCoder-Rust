76c6aa1ba9eacc81f84b2b1843c7265d6356eccc
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-05-06 21:17:04 +0900

    practice abc126 E
    連結グラフの数を求める問題
    AXi + AYi + Zi = 偶数
    でありZiの数がわかっているならAXi、AYiのどちらか一方が判明した場合もう片方も判明する。
    Ziが偶数ならばAXiとAYiはどちらも偶数(2)か奇数(1)、Ziが奇数ならばAXiもしくはAYiのどちらかが偶数でどちらかが奇数になる。
    X、Yでグラフを作成すると連結されている部分のXYはそれか1つが判明すればすべてがわかることになる。
    結果、連結グラフがいくつあるのかを数えれば正解になる。

---
+++ WaterLevel/abc_126_e/src/main.rs
@@ -0,0 +1,111 @@
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::HashMap;
+
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        #[allow(unused_variables)]
+        #[allow(unused_mut)]
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+ 
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+ 
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+ 
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+ 
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+ 
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+ 
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("parse error")
+    };
+}
+
+fn main() {
+    input!{
+        n: usize,
+        m: usize,
+        xyz: [(usize1, usize1, usize); m]
+    }
+    let mut graph = vec![vec![]; n];
+    for &(x, y, _) in &xyz {
+        graph[x].push(y);
+        graph[y].push(x);
+    }
+    let mut count = 0;
+    let mut dist = vec![false; n];
+    
+    for i in 0..n {
+        if dist[i] {continue; }
+        dist[i] = true;
+        count += 1;
+        let mut queue = VecDeque::new();
+        queue.push_back(i);
+        while let Some(x) = queue.pop_front() {
+            for &p in &graph[x] {
+                if dist[p] {continue;}
+                dist[p] = true;
+                queue.push_back(p);
+            }
+        }
+    }
+    println!("{}", count);
+}
